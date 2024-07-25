cba5e2cd06a323059d522cc25272c1a6f47713f3
wmd23 <135710449+wmd23@users.noreply.github.com>
2024-07-25 18:14:04 -0300

    Update search.jl

--- search.jl
+++ search.jl
@@ -1,135 +1,46 @@
 # Reference: Yanyun Ding, Yunhai Xiao & Jianwei Li (2017) A class of conjugate gradient methods for convex
 # constrained monotone equations, Optimization, 66:12, 2309-2328, DOI:10.1080/02331934.2017.1372438
 
-using LinearAlgebra, Printf
-
-#step 0
-function solver(x0, F, proj; ξ = 1.0, σ = 1.0e-4, ρ = 0.74, η = 0.9, θ = 0.9, ϵ = 1.0e-5)
-
-    # inicializing variables
-    k = 0;                      # k is the number of iterations (end of step 0)
-    xkminusone = x0;            # xkminusone is the previous term of the sequence (xk)
-    xk = xkminusone;            # xk is the current term of the sequence (xk)
-    d = xk;                     # d is the direction vector
-    tk = ξ;                     # tk is the steplength parameter
-    zk = xk;                    # zk is the point that is projected in the hyperplane (for more details see the reference cited above)
-    t0 = time();                # t0 is the initial time
-    perror = 0;                 # if perror == 0 then the solution comes from the first stop condition and if perror == 1 then the second stop condition has achieved
-    fcounter = 0;               # stores the number of function evaluations
-    
-    # step 1
-    while norm(F(xk)) > ϵ       # first stop condition
-
-        #println("iter = $k   norm= $c")
-
-        # descent direction
-        if k == 0
-            d = -F(x0)
-            fcounter += 1
-        else
-            
-            Fxkminusone = F(xkminusone)
-            Fxk = F(xk)
-            c = norm(Fxkminusone)
-            fcounter += 2
-            
-            # y_(k-1) determinations
-            sk = tk*d
-            v = norm(sk)
-            γk = Fxk - Fxkminusone
-            λk = 1 + max(0,-dot(γk,sk)/v^2)/c
-            yk = γk+λk*tk*c*d
-
-            # βτk determination
-            τa = norm(yk)^2/dot(sk,yk)  
-            τb = dot(sk,yk)/v^2
-            τk = θ*τa + (1-θ)*τb
-            βτk = dot(Fxk,yk)/dot(d,yk)-(τk+τa-τb)*(dot(Fxk,sk)/dot(d,yk))
-
-            # βk determination
-            βk = max(βτk,η*(dot(Fxk,d)/norm(d)^2))
-
-            # dk determination
-            d = -Fxk + βk*d
-
-            xkminusone = xk
-            tk = ξ
-
-        end
-
-        # tk determination
-        p = norm(d)
-
-        # step 2
-        while dot(-F(xk+tk*d),d) < σ*tk*p^2
-            tk = ρ*tk
-            fcounter += 1
-        end
-
-        zk = xk + tk*d
-        Fzk = F(zk)
-        nFzk = norm(Fzk)
-        fcounter += 1
-
-        # step 3
-        if nFzk < ϵ       #colocar código de erro
-            et = time() - t0
-            perror = 1;
-            k += 1
-            return k, et, fcounter, zk, Fzk, nFzk, perror
-        else
-            αk = dot(Fzk,xk-zk)/nFzk^2
-            xk = proj(xk-αk*Fzk)
-        end
-
-        # step 4
-        k += 1
-
-    end
-    
-    et = time() - t0
-    Fxk = F(xk)
-    fcounter += 1
-    return k, et, fcounter, xk, Fxk, norm(Fxk), perror
-
-end
-
-function ding(x0, F, proj;xi=0.9,sigma=1.e-5,rho=0.74,eta=0.1,theta=0.9,maxiter=10000,tol=1.e-5)
+function ding(x0, F, proj;xi=0.9,sigma=1.e-5,rho=0.74,eta=0.1,theta=0.1,maxiter=1000,tol=1.e-5)
     
     k=0
     t0 = time()
+    Fevals = 0
 
     # step 1 (k=0)
     Fx0 = F(x0)
-    nFx0 = norm(Fx0)
+    Fevals +=1
+    nFx0 = sqrt(Fx0'*Fx0)
     if nFx0 < tol
         et = time() - t0
-        return x0, k, et, nFx0, 0
+        return x0, k, et, nFx0, Fevals, 0
     else
         dk = -Fx0
     end
 
     # step 2 (k=0)
     tk=xi
-    ndk=norm(dk)
-    while -F(x0+tk*dk)'*dk < sigma*tk*ndk^2
+    ndk=sqrt(dk'*dk)
+    e=sigma*ndk^2
+    while -F(x0+tk*dk)'*dk < e*tk
         tk = rho*tk
+        Fevals +=1
     end
     zk=x0+tk*dk
 
     # step 3 (k=0)
     Fzk=F(zk)
-    nFzk=norm(Fzk)
+    Fevals +=1
+    nFzk=sqrt(Fzk'*Fzk)
     if nFzk < tol
-        #if proj(zk)==zk
-            et = time() - t0
-            return x0, k, et, nFx0, 1
-        #end
+        et = time() - t0
+        return x0, k, et, nFx0, Fevals, 1
     else
         alphak=(Fzk'*(x0-zk))/nFzk^2
         x1=proj(x0-alphak*Fzk)
         Fx1=F(x1)
-        nFx1=norm(Fx1)
+        Fevals +=1
+        nFx1=sqrt(Fx1'*Fx1)
     end
 
     # step 4 (k=0)
@@ -137,20 +48,19 @@ function ding(x0, F, proj;xi=0.9,sigma=1.e-5,rho=0.74,eta=0.1,theta=0.9,maxiter=
 
     while k<maxiter
         
-        @printf("%5d  %20.15e %20.15e\n",k,nFx1,tk)
         # step 1 (k>0)
         if nFx1 < tol
             et = time() - t0
-            return x1, k, et, nFx1, 0
+            return x1, k, et, nFx1, Fevals, 0
         else
             gammak=Fx1-Fx0
             sk = tk*dk
-            nsk=norm(sk)
+            nsk=sqrt(sk'*sk)
             lambdak=1.0+max(0.0,-(gammak'*sk)/nsk^2)/nFx0
             yk=gammak+lambdak*nFx0*sk
             c=sk'*yk
             d=dk'*yk
-            tauA=norm(yk)^2/c
+            tauA=(yk'*yk)/c
             tauB=c/nsk^2
             tauk=theta*tauA+(1.0-theta)*tauB
             betak=(Fx1'*yk)/d-(tauk+tauA-tauB)*(Fx1'*sk)/d
@@ -164,28 +74,27 @@ function ding(x0, F, proj;xi=0.9,sigma=1.e-5,rho=0.74,eta=0.1,theta=0.9,maxiter=
         nFx0=nFx1
 
         tk=xi
-        ndk=norm(dk)
-        while -F(x0+tk*dk)'*dk < sigma*tk*ndk^2
+        ndk=sqrt(dk'*dk)
+        e=sigma*ndk^2
+        while -F(x0+tk*dk)'*dk < e*tk
             tk = rho*tk
+            Fevals +=1
         end
         zk=x0+tk*dk
 
         # step 3 (k>0)
         Fzk=F(zk)
-        nFzk=norm(Fzk)
+        Fevals +=1
+        nFzk=sqrt(Fzk'*Fzk)
         if nFzk < tol
             et = time() - t0
-            return x0, k, et, nFzk, 1
-        #end
-            # if proj(zk)==zk
-            #     et = time() - t0
-            #     return zk, k, et, nFzk, 1
-            # end
+            return x0, k, et, nFx0, Fevals, 1
         else
             alphak=(Fzk'*(x0-zk))/nFzk^2
             x1=proj(x0-alphak*Fzk)
             Fx1=F(x1)
-            nFx1=norm(Fx1)
+            Fevals +=1
+            nFx1=sqrt(Fx1'*Fx1)
         end
 
         # step 4 (k=0)
@@ -193,6 +102,6 @@ function ding(x0, F, proj;xi=0.9,sigma=1.e-5,rho=0.74,eta=0.1,theta=0.9,maxiter=
     end
 
     et = time() - t0
-    return x1, k, et, nFx1, 2
+    return x1, k, et, nFx1, Fevals, 2
 
 end
