0b3370ccf3f2d73c44d69068fb129c366bd0e6cc
wmd23 <135710449+wmd23@users.noreply.github.com>
2024-07-25 18:06:25 -0300

    Update main.jl
    Final version of main.jl

--- main.jl
+++ main.jl
@@ -1,27 +1,8 @@
 include("procedures.jl"); include("testfunctions.jl"); include("projections.jl")
-using CUTEst, NLPModels
-#A, B, C, p, q, r = myresults(gradsphere, id, factor =50)
 
-#nlp = CUTEstModel("PENALTY1","-param","N=100")
+x0 = ones(100) # guess
+F = gradsumsquares # vector field
+proj = projRplus # projection
 
-
-x0 = -1.0*ones(90)
-
-function F(x)
-    #return grad(nlp,x)
-    
-    n = length(x)
-    G = Vector{Float64}(undef,n)
-
-    for i in 1 : n
-        G[i] = 2. * (n - i + 1.) * x[i]
-    end
-
-    return G
-
-end
-
-s,iters,et,norm_F_x, error = ding(x0, F, id)
-
-
-finalize(nlp)
+# x,k,t,nFx,Fevals,erro=ding(x0,F,proj, maxiter=1.e4) 
+# use the line above if you want to solve the equation F(x)=0 for more details see Readme.md
