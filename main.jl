a5beaaa2afa57bfeed21b7c41de908cf3c5727ae
wmd23 <135710449+wmd23@users.noreply.github.com>
2024-07-26 09:25:36 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,8 +1,7 @@
 include("testfunctions.jl"); include("projections.jl")
 
 x0 = ones(100) # guess
-F = gradsumsquares # vector field
-proj = projRplus # projection
+F = gradsumsquares # see testfunctions.jl for more details
+proj = projRplus # see projections.jl for more details
 
-# x,k,t,nFx,Fevals,erro=ding(x0,F,proj, maxiter=1.e4) 
-# use the line above if you want to solve the equation F(x)=0 for more details see Readme.md
+x,k,t,nFx,Fevals,erro=ding(x0,F,proj, maxiter=1.e4) # see search.jl for more details
