e5810a3ab6b6de9fa7af2797142974c97400895d
wmd23 <135710449+wmd23@users.noreply.github.com>
2024-07-26 10:51:58 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,4 +4,4 @@ x0 = ones(100) # guess
 F = gradsumsquares # see testfunctions.jl for more details
 proj = projRplus # see projections.jl for more details
 
-x,k,t,nFx,Fevals,erro=ding(x0,F,proj, maxiter=1.e4) # see search.jl for more details
+x,k,t,nFx,Fevals,error=ding(x0,F,proj, maxiter=1.e4) # see search.jl for more details
